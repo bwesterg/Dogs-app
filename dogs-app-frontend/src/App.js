6f2c8b7ac18a25a0dfb678be257d7e7698a6584c
bwesterg <benton.westergaard@gmail.com>
2024-02-22 00:41:37 -0700

    adds new dog form and persisting create and delete routes

--- dogs-app-frontend/src/App.js
+++ dogs-app-frontend/src/App.js
@@ -1,4 +1,4 @@
-import react, {Component} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import DogContainer from './components/DogContainer';
 import DogForm from './components/DogForm';
@@ -19,12 +19,34 @@ class App extends Component {
       .then(dogs => this.setState({dogs})) 
   }
 
+  addDog = (newDog) => {
+    this.setState({
+      dogs: [...this.state.dogs, newDog]
+    })
+
+    fetch(dogsUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDog)
+    })
+  }
+
+  deleteDog = (id) => {
+    let filtered = this.state.dogs.filter(dog => dog.id !== id)
+    this.setState({
+      dogs: filtered
+    })
+    fetch(dogsUrl + "/" + id, { method: "DELETE" } )
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Dogs App</h1>
-        <DogForm />
-        <DogContainer dogs={this.state.dogs}/>
+        <DogForm addDog={this.addDog}/>
+        <DogContainer deleteDog={this.deleteDog} dogs={this.state.dogs}/>
       </div>
     );
   }
--- dogs_app_backend/app/controllers/dogs_controller.rb
+++ dogs_app_backend/app/controllers/dogs_controller.rb
@@ -3,4 +3,22 @@ class DogsController < ApplicationController
         @dogs = Dog.all
         render json: @dogs
     end
+
+    def create
+        @dog = Dog.create(
+            name: params[:name],
+            description: params[:description],
+            age: params[:age],
+            washed: params[:washed],
+            exercised: params[:exercised]
+        )
+
+        render json: @dog, status: :created
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        render status: :no_content
+    end
 end
